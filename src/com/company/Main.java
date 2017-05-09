5fcff2e038cda11bb2508d3f4c72b657d9bccd5a
gbarth1 <grace.m.barth@gmail.com>
2017-05-09 14:28:21 -0400

    kjlh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
-
+import java.util.*;
+import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String fileName = "rawDataU6.txt";
+        String line = null;
+        try{
+            FileReader filereader= new FileReader(fileName);
+            BufferedReader bufferedreader = new BufferedReader(filereader);
+            while((line=bufferedreader.readLine()) != null){
+
+            }
+            bufferedreader.close();
+        }
+        catch(FileNotFoundException ex){
+            System.out.println("file not found");
+        }
+        catch(IOException ex){
+            System.out.println("error reading file");
+        }
     }
 }
